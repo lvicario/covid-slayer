8d387e62476d66d5921fa2fdafeb9b67ed98787b
Leandro Vicario <leandro.vicario@gmail.com>
2020-09-13 20:05:03 +0800

    Updates

---
+++ precache-manifest.00b1f997a6ff01f407e579befac268d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66094da19e5537537f9d11b2aaf9b026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fc6972e2a6eccd3dccf",
+    "url": "/static/js/2.ceb184cd.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.ceb184cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac9590066bd259e4474f",
+    "url": "/static/js/main.77b5b248.chunk.js"
+  },
+  {
+    "revision": "78ffadc0bb5931f4cc72",
+    "url": "/static/js/runtime-main.5ee7748e.js"
+  }
+]);
